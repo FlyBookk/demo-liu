f419e462d16b0a16f9545cb39c99dee8e2ad33fb
liuzequan <liuzequan@lemoncome.com>
2019-11-27 17:12:29 +0800

    redis-test

---
+++ src/main/java/com/liuzq/web/RedisTestController.java
@@ -0,0 +1,66 @@
+package com.liuzq.web;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ProjectName: springboot-liuzq
+ * @Package: com.liuzq.web
+ * @ClassName: RedisTestController
+ * @Author: liuzq
+ * @Description:
+ * @Date: 2019/11/27 15:40
+ * @Version: 1.0
+ */
+@RestController
+public class RedisTestController {
+    static Logger logger= LoggerFactory.getLogger(RedisTestController.class);
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("/testR")
+    public void get(){
+        Object o = setHashKeyAndGet();
+        logger.info("获取key:{}", JSON.toJSONString(o));
+        logger.info("获取hashkey:{}", JSON.toJSONString(getHashValue()));
+    }
+
+    public  Object setKeyAndGet(String key,String value){
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set(key, value, 1, TimeUnit.MINUTES);//1分钟过期
+        return ops.get(key);
+    }
+
+    public String getValue(String key){
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        return ops.get(key);
+    }
+
+    public  Object setHashKeyAndGet(){
+        HashOperations ops = this.redisTemplate.opsForHash();
+        ops.delete("redis","test4");
+        ops.put("redis","test1","testRedisHashValue1");
+        ops.put("redis","test2","testRedisHashValue1");
+        ops.put("redis","test3","testRedisHashValue3");
+        ops.put("redis","test3","testRedisHashValue4");
+        ops.put("redis","test5","testRedisHashValue5");
+        redisTemplate.expire("redis", 24, TimeUnit.HOURS);
+        return ops.entries("redis");
+    }
+    public Object getHashValue(){
+        HashOperations ops = this.redisTemplate.opsForHash();
+        return ops.get("redis","test1");
+    }
+
+
+}
--- src/test/java/com/liuzq/SpringbootMybatisApplicationTests.java
+++ src/test/java/com/liuzq/SpringbootMybatisApplicationTests.java
@@ -1,16 +1,35 @@
 package com.liuzq;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootMybatisApplicationTests {
-
+Logger LOGGER= LoggerFactory.getLogger(SpringbootMybatisApplicationTests.class);
 	@Test
 	public void contextLoads() {
+		JSONObject jsonObj=new JSONObject();
+		Map<String , Object> resultJsonObj=new HashMap<>();
+		String uid = null;
+		try {
+			String params = jsonObj.getString("params");
+			JSONObject jsonObject = StringUtils.isEmpty(params) ? null : JSONObject.parseObject(params);
+			JSONObject initResult = jsonObject == null ? null : jsonObject.getJSONObject("initResult");
+			uid = initResult == null ? "" : initResult.getString("uid");
+			resultJsonObj.put("uid", uid);
+		} catch (Exception e) {
+			LOGGER.error("获取uid失败,error: ", e);
+		}
 	}
 
 
